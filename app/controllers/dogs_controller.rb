5fbc40f5c2aa2cce8b17d194bde9c0a375802889
Jaimi Klein jaimiblankenship@gmail.com <jaimiblankenship@gmail.com>
2024-08-27 14:02:25 +0200

    added new and create to dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -7,12 +7,25 @@ class DogsController < ApplicationController
   end
 
   def new
+    @dog = Dog.new
   end
 
   def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age, :bio)
   end
 
 end
