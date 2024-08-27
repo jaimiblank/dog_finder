f6b50f3f39ba4b63c207a0a69fcc24d1668225a1
Jaimi Klein jaimiblankenship@gmail.com <jaimiblankenship@gmail.com>
2024-08-27 17:48:37 +0200

    added bookings controller, most of bookings new html, needs refinement

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_08_27_131655) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_152323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,10 +42,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_131655) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "dog_id"
+    t.bigint "user_id"
+    t.index ["dog_id"], name: "index_bookings_on_dog_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "dogs", force: :cascade do |t|
@@ -73,9 +77,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_131655) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "dogs"
+  add_foreign_key "bookings", "users"
   add_foreign_key "dogs", "users"
-
 end
