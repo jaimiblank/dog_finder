72e5dbf159c577b54664e62cf368aef299858cee
Lutz Ricardo Hirschbiegel @users.noreply.github.com <l.r_hirschbiegel@hotmail.de>
2024-08-28 16:35:32 +0200

    Server Problem with simple Form Solved

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,7 +6,17 @@ class BookingsController < ApplicationController
   end
 
   def create
-    raise
+    @booking = Booking.new(booking_params)
+    @dog = Dog.find(params[:dog_id])
+    @booking.dog = @dog
+    @booking.user = current_user
+    if @booking.save
+      raise
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      raise
+      redirect_to dogs_path
+    end
     #booking need
     #user
     #dog
@@ -14,19 +24,12 @@ class BookingsController < ApplicationController
     #booking.new
     #to that booking assign a dog and a user
     #save that booking
-    
-    @booking = Booking.new(booking_params)
-    if @booking.save
-      redirect_to @booking, notice: 'Booking was successfully created.'
-    else
-      render :new
-    end
   end
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to bookings_url, notice: 'Booking deleted.'
+    redirect_to new_dog_booking_path, notice: 'Booking deleted.'
   end
 
   private
