0460ba067a40be180ca320480c85e8ff79c8d879
Lutz Ricardo Hirschbiegel @users.noreply.github.com <l.r_hirschbiegel@hotmail.de>
2024-08-29 09:00:37 +0200

    After Login -> Dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  # Add this method
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
